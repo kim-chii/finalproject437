1c1312bc8bc4855a5ae67c2a284e08ae20fa4fb3
mitchellzehr <MitchellZehr@hotmail.com>
2017-06-11 15:03:12 -0700

    started working on like functionality

--- MovieReview/public/Movie/mvDetailController.js
+++ MovieReview/public/Movie/mvDetailController.js
@@ -1,8 +1,9 @@
 app.controller('mvDetailController',
- ['$scope', '$state', '$http', '$uibModal', 'notifyDlg', 'rvws', 'mv',
- function($scope, $state, $http, $uibM, nDlg, rvws, mv) {
+ ['$scope', '$state', '$http', '$uibModal', '$q', 'notifyDlg', 'rvws', 'mv',
+ function($scope, $state, $http, $uibM, $q, nDlg, rvws, mv) {
    $scope.rvws = rvws;
    $scope.mv = mv;
+   $scope.sentiment = {};
    console.log("Rvws: " + JSON.stringify($scope.rvws));
    console.log("Movie: " + JSON.stringify($scope.mv));
 
@@ -15,8 +16,36 @@ app.controller('mvDetailController',
       .then(function(response) {
          $scope.rvs = response.data;
       })
-      .catch(function() {
-         nDlg.show($scope, "You cannot review a movie twice", "Error");
+      .catch(function(err) {
+         if (err.data[0].tag === "dupReview") {
+            nDlg.show($scope, "You cannot review a movie twice", "Error");
+         } else {
+            nDlg.show($scope, "You must fill the required fields ", "Error");
+         }
       });
    };
+
+   var init = function() {
+      var queries = [];
+      rvws.forEach(function(rev, i) {
+         queries[i] = $http.get('/Rvws/' + rev.id + '/Sentiment');
+      });
+
+      $q.all(queries).then(function(results) {
+         results.forEach(function(rev) {
+            var emails = {};
+            Object.keys(rev.data).forEach(function(key){
+               var sentiment = rev.data[key];
+               emails[sentiment.username] = sentiment.sentiment;
+            });
+
+            $scope.sentiment[rev.data[0].revId] = emails;
+         });
+      });
+
+      console.log($scope.sentiment)
+
+   };
+
+   init();
  }]);
